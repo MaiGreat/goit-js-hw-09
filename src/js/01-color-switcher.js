e67a11d91d7dc3b15c24c12a95165281950de234
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-04-09 13:38:31 +0200

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+const findButtonElStart = document.querySelector('button[data-start]');
+const findButtonElStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+
+
+findButtonElStart.addEventListener('click', onChangeColor);
+findButtonElStop.addEventListener('click', offChangeColor);
+
+findButtonElStop.setAttribute('disabled', true);
+
+function onChangeColor(evt) {
+        findButtonElStart.setAttribute('disabled', true);
+        findButtonElStop.removeAttribute('disabled')
+
+    timerId = setInterval(() => {
+    const changeColor = getRandomHexColor();
+    
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    };
+        bodyEl.style.backgroundColor = changeColor;
+  }, 1000);
+}
+
+function offChangeColor(evt) {
+    
+    findButtonElStart.removeAttribute('disabled');
+    findButtonElStop.setAttribute('disabled', true);
+    
+    bodyEl.style.backgroundColor = '#fafafa';
+    clearInterval(timerId);
+}
+
